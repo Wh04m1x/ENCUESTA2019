724bb4c8d3c13803c89e5f44a2204d2c06860584
Helpdesk Agros <helpdesk@agros.mx>
2019-09-12 08:04:01 -0500

    eliminar basura

--- administrador/resultados/resultados.php
+++ administrador/resultados/resultados.php
@@ -109,6 +109,25 @@ $row3 = $resultados3->fetch_assoc();
                     <!-- Content Row -->
 
                     <div class="row">
+
+                    <div class="col-xl-12 col-lg-7">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="chart-area">
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Resultados</h6>
+                </div>
                         <?php
 
 $consulta = "SELECT * FROM preguntas WHERE id_encuesta = '$id_encuesta' AND ver = '0'";
